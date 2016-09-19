890aaa64ff206d5e077d2e7e782b6b258255ba5c
Katherine Nagels <knfrances@gmail.com>
2016-09-19 14:18:15 +1200

    Work on checkForWin function

--- minesweeper.js
+++ minesweeper.js
@@ -17,31 +17,51 @@ var board = {
 };
 
 function startGame () {
+
   // Loop through the contents of board.cells (i.e, each cell)
   for (var i = 0; i < board.cells.length; i++) {
     // Call countSurroundingMines once for each cell in board.cells
     // Assign the result of countSurroundingMines to a property (surroundingMines) on each cell object.
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
   }
+
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
+
+  // In startGame, use document.addEventListener to call checkForWin every time the left mouse button is clicked.
+  document.addEventListener("click", checkForWin);
+
+  // Add another event listener that calls checkForWin when the right mouse button is clicked.
+  document.addEventListener("contextmenu", checkForWin);
 }
 
 // Define this function to look for a win condition:
-//
+
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
+
 function checkForWin () {
 
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+  // Loop through all of board.cells
+  for (var i = 0; i < board.cells.length; i++) {
+
+    // Check to see if all mines are marked. If not, return
+    if (board.cells[i].isMine === true && board.cells[i].isMarked !== true) {
+      return;
+    }
+
+    // If any cells are still hidden, game is not won
+    else if (board.cells[i].hidden === true) {
+      return;
+    }
+  }
+  // You can use this function call to declare a winner (once you've detected that they've won, that is!)
+  lib.displayMessage('You win!');
 }
+// THE ABOVE DOESN'T WORK!!!!!!
 
-// Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`:
 
+// Define this function to count the number of mines around the cell (there could be as many as 8). You don't have to get the surrounding cells yourself! Just use `lib.getSurroundingCells`:
 // var surrounding = lib.getSurroundingCells(cell.row, cell.col)
 
 // Note: lib.getSurroundingCells returns a subset of the `cells` array, including only those cells which are adjacent to `row`, `col`
